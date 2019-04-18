791cb8f2cbdcf8e654a57b49a1e53e90057bb406
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-04-18 22:09:03 +0300

    Fix linter

--- code/04-server/02-path-and-arguments.py
+++ code/04-server/02-path-and-arguments.py
@@ -2,11 +2,11 @@ from aiohttp import web
 
 
 async def handler(request: web.Request) -> web.Response:
-    user = request.match_info.get('user', 'Anonimous')
+    user = request.match_info.get("user", "Anonimous")
     return web.Response(text=f"Hello, {user}")
 
 
-async def init_app():
+async def init_app() -> web.Application:
     app = web.Application()
     app.add_routes([web.get("/", handler)])
     app.add_routes([web.get("/{user}", handler)])
