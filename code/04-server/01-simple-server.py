42159c18d85e51d93daa68fa432d70a3bae08ca6
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-04-18 15:26:03 +0300

    Add simple server tutorial

---
+++ code/04-server/01-simple-server.py
@@ -0,0 +1,15 @@
+from aiohttp import web
+
+
+async def handler(request: web.Request) -> web.Response:
+    return web.Response(text="OK")
+
+
+
+async def init_app():
+    app = web.Application()
+    app.add_routes([web.get('/', handler)])
+    return app
+
+
+web.run_app(init_app())
