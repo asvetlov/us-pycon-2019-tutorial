7d15d994206c4b2fd76870351f3cab1068c94e88
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-04-21 15:01:50 +0300

    Add example for path arguments (#2)
    * Add example for path arguments
    
    * Trigger build
    
    * Fix linter

---
+++ code/04-server/02-path-and-arguments.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+
+
+async def handler(request: web.Request) -> web.Response:
+    user = request.match_info.get("user", "Anonimous")
+    return web.Response(text=f"Hello, {user}")
+
+
+async def init_app() -> web.Application:
+    app = web.Application()
+    app.add_routes([web.get("/", handler)])
+    app.add_routes([web.get("/{user}", handler)])
+    return app
+
+
+web.run_app(init_app())
