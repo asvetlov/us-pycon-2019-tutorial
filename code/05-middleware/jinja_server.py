581d71ba3b423ecfc6bc117e66e2b66e13dcbe09
Mariatta <mariatta@python.org>
2019-04-24 17:01:38 -0700

    lint

--- code/05-middleware/jinja_server.py
+++ code/05-middleware/jinja_server.py
@@ -1,21 +1,22 @@
-import jinja2
-import aiohttp_jinja2
 import os
 
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
+
 
 routes = web.RouteTableDef()
 
-@routes.get('/{username}')
-async def greet_user(request):
+
+@routes.get("/{username}")
+async def greet_user(request: web.Request) -> web.Response:
 
     context = {
-        'username': request.match_info.get("username", ""),
-        'current_date': 'January 27, 2017'
+        "username": request.match_info.get("username", ""),
+        "current_date": "January 27, 2017",
     }
     template = "base.html"
-    response = aiohttp_jinja2.render_template(template, request,
-                                              context=context)
+    response = aiohttp_jinja2.render_template(template, request, context=context)
     return response
 
 
@@ -24,12 +25,10 @@ async def init_app() -> web.Application:
     app.add_routes(routes)
 
     aiohttp_jinja2.setup(
-        app,
-        loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), "templates"))
+        app, loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(), "templates"))
     )
 
     return app
 
 
 web.run_app(init_app())
-
